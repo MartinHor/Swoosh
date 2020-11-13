b617608daf938c6ceac88d6e8a17afbb455296c1
MartinUcsi <martinhor5578@gmail.com>
2020-11-13 21:20:33 +0800

    lesson 3

--- app/src/main/java/com/martinparker/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/martinparker/swoosh/WelcomeActivity.kt
@@ -1,12 +1,24 @@
 package com.martinparker.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, activity_league::class.java)
+            startActivity(leagueIntent)
+
+        }
+
     }
+
+
+
 }
