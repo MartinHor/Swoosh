602f461b73671c5b00c4105ec75eb7989cae46f7
MartinUcsi <martinhor5578@gmail.com>
2020-11-14 16:14:27 +0800

    lesson8

--- app/src/main/java/com/martinparker/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/martinparker/swoosh/LeagueActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +17,40 @@ class LeagueActivity : BaseActivity() {
 
 
     }
+
+    fun onMensClicked(view: View) {
+        womensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+
+    fun onCoedClicked(view: View) {
+        mensLeagueBtn.isChecked = false
+        womensLeagueBtn.isChecked = false
+
+        selectedLeague = "co-ed"
+
+    }
+
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillAcitivity::class.java)
-        startActivity(skillActivity)
+        if (selectedLeague != ""){
+            val skillActivity = Intent(this, SkillAcitivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 
 }
--- app/src/main/java/com/martinparker/swoosh/SkillAcitivity.kt
+++ app/src/main/java/com/martinparker/swoosh/SkillAcitivity.kt
@@ -2,11 +2,17 @@ package com.martinparker.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class SkillAcitivity : AppCompatActivity() {
+class SkillAcitivity : BaseActivity() {
+
+    var league = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill_acitivity)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
+
 }
